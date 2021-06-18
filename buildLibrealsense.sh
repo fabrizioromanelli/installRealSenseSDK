ddaa6f444a7fad880d28e7891542af623ca38282
Roberto Masocco <robmasocco@gmail.com>
2021-06-18 15:55:38 +0200

    Various fixes to make this work on both Nanos and NX with CUDA 11.

--- buildLibrealsense.sh
+++ buildLibrealsense.sh
@@ -1,28 +1,30 @@
 #!/bin/bash
-# Builds the Intel Realsense library librealsense on a Jetson Nano Development Kit
-# Copyright (c) 2016-19 Jetsonhacks 
+
+# Builds the Intel Realsense library librealsense on a Jetson board.
+# Copyright (c) 2016-19 Jetsonhacks
+# Rearranged by Fabrizio Romanelli, Roberto Masocco, June 2021
 # MIT License
 
-# Jetson Nano; L4T 32.2.3
-
-LIBREALSENSE_DIRECTORY=~/thorDrone/Tools_Workspace/librealsense
+LIBREALSENSE_DIRECTORY=$1/Tools_Workspace/librealsense
 LIBREALSENSE_VERSION=v2.43.0
+INIT_DIR=$1
 INSTALL_DIR=$PWD
-NVCC_PATH=/usr/local/cuda-10.2/bin/nvcc
+NVCC_PATH=/usr/local/cuda/bin/nvcc
 
 USE_CUDA=true
 
+# Shows help text.
 function usage
 {
-    echo "usage: ./buildLibrealsense.sh [[-c ] | [-h]]"
-    echo "-nc | --build_with_cuda  Build no CUDA (Defaults to with CUDA)"
-    echo "-h | --help  This message"
+    echo "usage: ./buildLibrealsense.sh ROOT_DIR [[-c ] | [-h]]"
+    echo "-nc | --no-cuda  Build without CUDA support"
+    echo "-h | --help Display this message"
 }
 
 # Iterate through command line inputs
 while [ "$1" != "" ]; do
     case $1 in
-        -nc | --build_no_cuda )  USE_CUDA=false
+        -nc | --no-cuda )  USE_CUDA=false
                                 ;;
         -h | --help )           usage
                                 exit
@@ -34,18 +36,11 @@ while [ "$1" != "" ]; do
 done
 
 echo "Build with CUDA: "$USE_CUDA
-sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-8 10
-sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-9 20
-sudo update-alternatives --config gcc
-sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-8 10
-sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-9 20
-sudo update-alternatives --config g++
 
+# Output colors.
 red=`tput setaf 1`
 green=`tput setaf 2`
 reset=`tput sgr0`
-# e.g. echo "${red}The red tail hawk ${green}loves the green grass${reset}"
-
 
 echo ""
 echo "Please make sure that no RealSense cameras are currently attached"
@@ -53,18 +48,18 @@ echo ""
 read -n 1 -s -r -p "Press any key to continue"
 echo ""
 
-if [ ! -d "$LIBREALSENSE_DIRECTORY" ] ; then
-  # clone librealsense
-  cd ~/thorDrone/Tools_Workspace
+if [[ ! -d "$LIBREALSENSE_DIRECTORY" ]]; then
+  # Clone librealsense.
+  cd $INIT_DIR/Tools_Workspace
   echo "${green}Cloning librealsense${reset}"
   git clone https://github.com/IntelRealSense/librealsense.git
 fi
 
-# Is the version of librealsense current enough?
+# Is the version of librealsense correct?
 cd $LIBREALSENSE_DIRECTORY
 VERSION_TAG=$(git tag -l $LIBREALSENSE_VERSION)
-if [ ! $VERSION_TAG  ] ; then
-   echo ""
+if [[ ! $VERSION_TAG  ]]; then
+  echo ""
   tput setaf 1
   echo "==== librealsense Version Mismatch! ============="
   tput sgr0
@@ -81,60 +76,61 @@ git checkout $LIBREALSENSE_VERSION
 
 # Install the dependencies
 cd $INSTALL_DIR
-sudo ./scripts/installDependencies.sh
+./scripts/installDependencies.sh
 
 cd $LIBREALSENSE_DIRECTORY
-git checkout $LIBREALSENSE_VERSION
 
 # Now compile librealsense and install
 mkdir build 
 cd build
 # Build examples, including graphical ones
-echo "${green}Configuring Make system${reset}"
+echo "${green}Configuring build system...${reset}"
 # Build with CUDA (default), the CUDA flag is USE_CUDA, ie -DUSE_CUDA=true
 export CUDACXX=$NVCC_PATH
 export PATH=${PATH}:/usr/local/cuda/bin
 export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/usr/local/cuda/lib64
 
-/usr/bin/cmake ../ -DBUILD_EXAMPLES=true -DFORCE_LIBUVC=true -DBUILD_WITH_CUDA="$USE_CUDA" -DCMAKE_BUILD_TYPE=release -DBUILD_PYTHON_BINDINGS=bool:true
+cmake ../ -DBUILD_EXAMPLES=true -DFORCE_LIBUVC=true -DBUILD_WITH_CUDA="$USE_CUDA" -DCMAKE_BUILD_TYPE=release -DBUILD_PYTHON_BINDINGS=bool:true
 
 # The library will be installed in /usr/local/lib, header files in /usr/local/include
 # The demos, tutorials and tests will located in /usr/local/bin.
 echo "${green}Building librealsense, headers, tools and demos${reset}"
+echo "${green}Leaving one CPU out to avoid blocking the system...${reset}"
 
-NUM_CPU=$(nproc)
+NUM_CPU=$(nproc --all)
 time make -j$(($NUM_CPU - 1))
-if [ $? -eq 0 ] ; then
-  echo "librealsense make successful"
+if [[ $? -eq 0 ]]; then
+  echo "${green}librealsense build successful!${reset}"
 else
   # Try to make again; Sometimes there are issues with the build
   # because of lack of resources or concurrency issues
-  echo "librealsense did not build " >&2
-  echo "Retrying ... "
+  echo "${red}librealsense did not build${reset}" 1>&2
+  echo "Retrying..."
   # Single thread this time
   time make 
-  if [ $? -eq 0 ] ; then
-    echo "librealsense make successful"
+  if [[ $? -eq 0 ]]; then
+    echo "${green}librealsense build successful!${reset}"
   else
-    # Try to make again
-    echo "librealsense did not successfully build" >&2
-    echo "Please fix issues and retry build"
+    # Try one last time
+    echo "${red}librealsense did not successfully build${reset}" 1>&2
+    echo "${red}Please fix issues and retry build${reset}" 1>&2
     exit 1
   fi
 fi
-echo "${green}Installing librealsense, headers, tools and demos${reset}"
+echo "${green}Installing librealsense, headers, tools and demos...${reset}"
 sudo make install
-  
-if  grep -Fxq 'export PYTHONPATH=$PYTHONPATH:/usr/local/lib' ~/.bashrc ; then
-    echo "PYTHONPATH already exists in .bashrc file"
+
+# Add Python wrapper to PATH.
+if grep -Fxq 'export PYTHONPATH=$PYTHONPATH:/usr/local/lib' ~/.bashrc ; then
+    echo "PYTHONPATH already exists in .bashrc file!"
 else
    echo 'export PYTHONPATH=$PYTHONPATH:/usr/local/lib' >> ~/.bashrc 
    echo "PYTHONPATH added to ~/.bashrc. Pyhon wrapper is now available for importing pyrealsense2"
 fi
 
+# Copy over the udev rules so that camera can be run from user space
 cd $LIBREALSENSE_DIRECTORY
 echo "${green}Applying udev rules${reset}"
-# Copy over the udev rules so that camera can be run from user space
 sudo cp config/99-realsense-libusb.rules /etc/udev/rules.d/
 sudo udevadm control --reload-rules && udevadm trigger
 
@@ -147,6 +143,3 @@ echo "The demos and tools are located in /usr/local/bin"
 echo " "
 echo " -----------------------------------------"
 echo " "
-
-
-
--- scripts/installDependencies.sh
+++ scripts/installDependencies.sh
@@ -1,26 +1,25 @@
 #!/bin/bash
-# installDependencies.sh
-# Install dependencies for  the Intel Realsense library librealsense2 on a Jetson Nano Developer Kit
+
+# Install dependencies for  the Intel Realsense library librealsense2 on a Jetson Board
 # Copyright (c) 2016-19 Jetsonhacks 
+# Rearranged by Fabrizio Romanelli, Roberto Masocco, June 2021
 # MIT License
+
+# Output colors.
 red=`tput setaf 1`
 green=`tput setaf 2`
 reset=`tput sgr0`
-# e.g. echo "${red}red text ${green}green text${reset}"
-echo "${green}Adding Universe repository and updating${reset}"
-apt-add-repository universe
-apt-get update
-echo "${green}Adding dependencies, graphics libraries and tools${reset}"
-apt-get install libssl-dev libusb-1.0-0-dev pkg-config -y
-# This is for ccmake
-apt-get install build-essential cmake cmake-curses-gui -y
 
-# Graphics libraries - for SDK's OpenGL-enabled examples
-apt-get install libgtk-3-dev libglfw3-dev libgl1-mesa-dev libglu1-mesa-dev -y
+# Generic stuff.
+echo "${green}Adding Universe repository and updating...${reset}"
+sudo apt-add-repository universe
+sudo apt-get update
+echo "${green}Adding dependencies, graphics libraries and tools...${reset}"
+sudo apt-get install libssl-dev libusb-1.0-0-dev pkg-config -y
+sudo apt-get install build-essential cmake cmake-curses-gui -y
 
-# QtCreator for development; not required for librealsense core library
-#apt-get install qtcreator -y
+# Graphics libraries - for SDK's OpenGL-enabled examples.
+sudo apt-get install libgtk-3-dev libglfw3-dev libgl1-mesa-dev libglu1-mesa-dev -y
 
-# Add Python 3 support
-apt-get install -y python3 python3-dev
-
+# Add Python 3 support.
+sudo apt-get install -y python3 python3-dev
